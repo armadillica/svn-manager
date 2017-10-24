d1a746e22ea41bb771d9dedbc58547da9a801a8a
Sybren A. Stüvel <sybren@stuvel.eu>
2017-10-24 10:30:53 +0200

    Added more logging for SVNMan.CreateRepo()

--- svnman/svnman.go
+++ svnman/svnman.go
@@ -1,12 +1,29 @@
 package svnman
 
-import "errors"
+import (
+	"errors"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// ErrNotImplemented is returned as error when a feature hasn't been implemented yet.
+var ErrNotImplemented = errors.New("SVNMan feature not implemented")
 
 // SVNMan provides SVN management operations.
 type SVNMan struct {
 }
 
 // CreateRepo creates a repository and Apache location directive.
-func (svn *SVNMan) CreateRepo(repoInfo CreateRepo) error {
-	return errors.New("not implemented")
+func (svn *SVNMan) CreateRepo(repoInfo CreateRepo, logFields log.Fields) error {
+	logger := log.WithFields(logFields).WithFields(log.Fields{
+		"repo_id":       repoInfo.RepoID,
+		"auth_realm":    repoInfo.AuthenticationRealm,
+		"project_id":    repoInfo.ProjectID,
+		"creator_name":  repoInfo.Creator.FullName,
+		"creator_email": repoInfo.Creator.Email,
+	})
+
+	logger.Info("going to create repository")
+	return ErrNotImplemented
+}
 }
